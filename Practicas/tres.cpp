9dc91527178ad6851f24ca613fd97bf6318ff2f5
JosueTorres23 <kevin.barreto35@my.unitec.edu.mx>
2024-10-21 19:36:03 -0600

    Create tres.cpp

---
+++ Practicas/tres.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+
+	/* Practica 3 */
+
+		//1.1 cuadrado de un numero
+	int cuadNum;
+	cout << "Escriba un numero del que quiera obtener el cuadrado ";
+	cin >> cuadNum;
+	cout << endl;
+
+	cout << "El cuadrado de " << cuadNum << " es " << cuadNum * cuadNum << endl;
+	cout << endl;
+
+	//1.2 Valor de X
+	int val;
+	cout << "Escriba el valor de x para x, 2x, 3x, 4x, 5x: ";
+	cin >> val;
+	cout << endl;
+	cout << val << " -- " << 2 * val << " -- " << 3 * val << " -- " << 4 * val << " -- " << 5 * val << endl;
+	cout << endl;
+
+	//1.3 Conersión de peso
+	int pesUs;
+	const double lib = 2.2;
+	cout << "Introduzca su peso para hacer la conversión a libras: ";
+	cin >> pesUs;
+	cout << endl;
+	cout << "Su peso en libras es de "<< pesUs*lib;
+	cout << endl;
+
+	//1.4 Promedio de tres números
+	int n1, n2, n3;
+	int tot = n1 + n2 + n3;
+	int prom;
+
+}
