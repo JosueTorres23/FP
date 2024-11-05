cf529a6aa22f0b1f97b7f32b712d722e5880c394
JosueTorres23 <kevin.barreto35@my.unitec.edu.mx>
2024-11-04 19:26:35 -0600

    Create cinco.cpp

---
+++ Practicas/cinco.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	//1.21 Pregunta para salir del código
+	string respuesta;
+	do
+	{
+	 cout<< "Quieres salir del ciclo?"<<"\n""\n";
+	 cin >> respuesta;
+
+	 if ( respuesta== "SI")
+	 {
+		 cout << "\n""\n";
+		 break;
+	 }
+	} while (true);
+
+	//1.22 Menu e opciones de operaciones
+	
+	//1.23 Cajero
+	float saldo,retiro;
+
+	saldo = saldo + 3450;
+
+	do
+	{
+		cout << "Menu de cajero, seleccione una opción" << "\n""\n";
+		cout << "Consulta de saldo"<<"\n";
+		cout << ""
+	} while (true);
+
+}
